5e4a010b0748d02c3b43a5b8e52f22a3e4c1d8e4
praseedapotnuru <82453459+praseedapotnuru@users.noreply.github.com>
2022-01-23 19:11:16 +0100

    Delete activity_2.js

--- activity_2.js
+++
@@ -1,8 +0,0 @@
-function getScore() {
-	score=localStorage.getItem("score");
-	document.getElementById("update").innerHTML=" <h1>Score :" +score +"</h1>";
-}
-
-function back() {
-	window.location="activity_1.html"
-}
